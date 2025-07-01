a4576ce449e37f14e555a82d098f28d90ae2c4ae
echo1 <coloveyx@gmial.com>
2025-07-01 12:48:19 +0500

    upd. App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,21 @@
 <script>
     import BrowserInfo from "./lib/BrowserInfo.svelte";
     import Update from "./lib/Update.svelte";
+    import Login from "./lib/Login.svelte";
+
+    let isLoggedIn = false;
+
+    function onLoginSuccess() {
+        isLoggedIn = true;
+    }
 </script>
 
-    <BrowserInfo />
-    <Update />
+{#if !isLoggedIn}
+  <Login on:LoginSuccess={onLoginSuccess}/>
+{:else}
+  <BrowserInfo />
+  <Update />
+{/if}
 
 <style>
     
